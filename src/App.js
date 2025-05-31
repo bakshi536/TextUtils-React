9edcbb5d5661e6d3abd2595d395446d00d60f69c
bakshihere <tavgunkbakshi@gmail.com>
2025-05-31 19:43:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Navbar from './components/Navbar';
+import About from './components/About';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+import { useState } from 'react';
+import {
+ BrowserRouter as Router, 
+ Routes,
+ Route,
+ Link
+} from "react-router-dom";
+
 function App() {
+  // const Object={
+  //   title:"juf",
+  //   aboutText:"bjg"
+  // }
+
+const [mode, setMode] = useState('light'); 
+const [alert, setalert] = useState(null);
+const [color, setcolor] = useState('white');
+
+
+const  toggleMode=()=>{
+  if(mode==='light'){
+    setMode('dark');
+    document.body.style.backgroundColor='black';
+    showAlert("Dark mode has been enabled.","success");
+    document.title='TextUtils-DarkMode';
+  }
+  else{
+    setMode('light');
+    document.body.style.backgroundColor='white';
+    showAlert("Light mode has been enabled.","success");
+     document.title='TextUtils-LightMode'
+  } 
+}
+
+const showAlert=(message,type)=>{
+setalert({
+  msg:message,
+  type:type
+})
+setTimeout(()=>{
+setalert(null);
+},1500);
+}
+
+const changeBlue=(r)=>{
+  setcolor('blue');
+  document.body.style.backgroundColor='blue';
+  showAlert("Blue Mode has been enabled.","success");
+}
+
+const changePink=()=>{
+    setcolor('pink');
+    document.body.style.backgroundColor='pink';
+    showAlert("Pink mode has been enabled.","success");
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+     {/* <Navbar props={
+      Object
+    }/>  */}
+    <Router>
+    <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} changeBlue={changeBlue} changePink={changePink}  showAlert={showAlert} color={color}/> 
+    <Alert alert={alert} />
+    <div className="container">
+    <Routes>
+          
+          <Route path="/" element={<TextForm value="Enter your text here" showAlert={showAlert} btn1="Convert to Uppercase" btn2="Convert to Lowercase" mode={mode}/>} />
+          <Route path="/about" element={<About />} />
+
+          </Routes>
     </div>
+    </Router>
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,44 @@
+import React, {useState} from 'react'
+
+export default function TextForm(props) {
+
+    const onUpClick=()=>{
+    let newText=text.toUpperCase(); 
+     setText(newText);
+     props.showAlert("Your text has been converted to UpperCase.","success");
+    }
+    const onLowClick=()=>{
+        let newText=text.toLowerCase(); 
+         setText(newText);
+         props.showAlert("Your text has been converted to LowerCase.","success");
+        }
+        const onClear=()=>{
+            let newText=""; 
+             setText(newText);
+             props.showAlert("Your textbox has been cleared.","success");
+            }
+    const onChangeHandle=(event)=>{
+     setText(event.target.value);
+    }
+    const [text, setText]=useState('');
+
+  return (
+    <>
+    <div className="container mb-3">
+  <h1 className={`my-3 text-${props.mode==='light' ? 'dark' : 'light'}`}>{props.value}</h1>
+  <textarea className="form-control my-4" value={text} onChange={onChangeHandle}id="TextForm" rows="8" style={{backgroundColor:props.mode==='light'?'white':'black' , color:props.mode==='light'?'black':'white'}} ></textarea>
+  <button className="btn btn-primary my-3" onClick={onUpClick}>{props.btn1}</button>
+  <button className="btn btn-primary my-3 mx-3" onClick={onLowClick}>{props.btn2}</button>
+  <button className="btn btn-primary my-3 mx-3" onClick={onClear}>Clear Text</button>
+</div>
+<div className={`container  text-${props.mode==='light' ? 'dark' : 'light'}`}>
+    <h2>Your text summary </h2>
+    <p>{text.split(" ").length} words and {text.length} charac</p>
+    <p>{text.length*0.008} Minutes Read</p>
+    <h3>Preview</h3>
+    <p>{text.length>0?text:"Enter something in textbox that you would like to preview"}</p>
+</div>
+    </>
+
+  )
+}
